e348ddefdf46d9c44017c4e888efa00180cad6c6
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2024-10-28 00:44:59 +0800

    Enhacnce readme and add diagram image

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
-<h1>Usage</h1>
-python3 -m venv venv
+# Usage
 
-source venv/bin/activate
+## To run the code:
+`python3 -m venv venv`
 
-pip install requirements.txt
+`source venv/bin/activate`
+
+`pip install requirements.txt`
+
+`fastapi dev server.py`
+
 
 Our code utilizes the pyaudio library. To use it, you have to install the portaudio library.
 
 for windows:
-sudo apt-get install portaudio19-dev
+`sudo apt-get install portaudio19-dev`
 
 for mac:
-brew install portaudio
+`brew install portaudio`
 
+### For Developers
+`pip freeze > requirements.txt` everytime there is updates to the requirements
 
-<h1>For Developers</h1>
-pip freeze > requirements.txt
+`fastapi dev server.py`
 
-fastapi dev server.py
+![alt text](CS3103Assignment4.png)
+
+## Current Progess
+[x] Sending over textual data
+[x] Established page with prof side, able to see data from student to prof page
+
+[ ] Need to send over actual audio data
+[ ] Need to get the students name at the start of loading the client
--- server.py
+++ server.py
@@ -1,19 +1,17 @@
 from typing import List
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse
 
 app = FastAPI()
-
-
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
         self.current_speaker = None  # Track the current speaker
-        self.speaking_time = 5
+        self.prof_websocket = None  # Track the professor's WebSocket connection
 
     async def connect(self, websocket: WebSocket):
-        print("Server has connected to websocket")
+        print("Server has connected to a student WebSocket")
         await websocket.accept()
         self.active_connections.append(websocket)
 
@@ -22,15 +20,22 @@ class ConnectionManager:
         if websocket == self.current_speaker:
             self.current_speaker = None  # Clear the speaker if they disconnect
 
+    async def connect_prof(self, websocket: WebSocket):
+        print("Server has connected to the professor WebSocket")
+        await websocket.accept()
+        self.prof_websocket = websocket  # Assign the WebSocket connection for the professor
+
+    async def disconnect_prof(self):
+        print("Professor WebSocket disconnected")
+        self.prof_websocket = None
+
     async def start_speaking(self, websocket: WebSocket):
-        print("Attempting to start speaking")
         if self.current_speaker is None:
             self.current_speaker = websocket
             return True
         return False
 
     async def stop_speaking(self, websocket: WebSocket):
-        print("Cancel speaking")
         if websocket == self.current_speaker:
             self.current_speaker = None
 
@@ -41,38 +46,66 @@ class ConnectionManager:
         for connection in self.active_connections:
             await connection.send_text(message)
 
-
 manager = ConnectionManager()
 
-
 @app.get("/")
-async def get():
+async def get_index():
     return FileResponse("index.html")
 
+@app.get("/prof")
+async def get_prof():
+    return FileResponse("prof.html")
 
 @app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_json()
-            action = data.get("action")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    if client_id == "prof":
+        await manager.connect_prof(websocket)
+        try:
+            while True:
+                # Keep the connection open for the professor
+                print("Professor WebSocket connected")
+                await websocket.receive_text()  
+        except WebSocketDisconnect:
+            await manager.disconnect_prof()
+            print("Professor WebSocket disconnected")
+        return
+    else:
+        await manager.connect(websocket)
+        
+        try:
+            while True:
+                print(f"Active student connections: {manager.active_connections}")
+                data = await websocket.receive_json()
+                action = data.get("action")
 
-            if action == "start":
-                # Check if the current user can start speaking
-                if await manager.start_speaking(websocket):
-                    # Allow speaking
-                    await websocket.send_json({"action": "start"})
-                else:
-                    print("sending deny")
-                    # Deny speaking
-                    await websocket.send_json({"action": "deny"})
+                if action == "start":
+                    # Check if the current user can start speaking
+                    if await manager.start_speaking(websocket):
+                        await websocket.send_json({"action": "start"})
+                        
+                        ### send over the information to the prof_websocket
+                        if manager.prof_websocket:
+                            print(f"Student {client_id} is speaking")
+                            await manager.prof_websocket.send_text(f"Student {client_id} is speaking")
+                    else:
+                        await websocket.send_json({"action": "deny"})
 
-            elif action == "stop":
-                print("action to stop speaking")
-                # Stop speaking and clear the current speaker
-                await manager.stop_speaking(websocket)
+                elif action == "stop":
+                    await manager.stop_speaking(websocket)
+                    if manager.prof_websocket:
+                        await manager.prof_websocket.send_text(f"Student {client_id} stopped speaking")
 
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} disconnected")
+        except WebSocketDisconnect:
+            manager.disconnect(websocket)
+            await manager.broadcast(f"Student #{client_id} disconnected")
+
+# @app.websocket("/ws/prof")
+# async def websocket_prof_endpoint(websocket: WebSocket):
+#     await manager.connect_prof(websocket)
+#     try:
+#         while True:
+#             # Keep the connection open for the professor
+#             await websocket.receive_text()  
+#     except WebSocketDisconnect:
+#         await manager.disconnect_prof()
+#         print("Professor WebSocket disconnected")
